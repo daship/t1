17269d2e163557dad4b777a58986cfaee7ccc0da
LAPTOP-TQN128LP\Ace <daship1@gmail.com>
2017-08-17 10:25:26 +0800

    pull data from github and push into github

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //say something
-            int a = 1;
+            int number = 1;
             Console.Read();
             
         }
