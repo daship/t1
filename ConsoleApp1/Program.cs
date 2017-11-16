6b258feefe015d3ac3e1b6716fdad3ad6286b61e
Ace.Lee <daship1@gmail.com>
2017-11-16 16:27:54 +0800

    back to init

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,31 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string input = "ace_17";
-            string[] arr = input.Split('_');
-
-            int count = Convert.ToInt32(arr[1]);
-
-            Console.WriteLine(add(count, count) + multify(count, 2));
-
+            Console.Write("init");
             Console.Read();
         }
-
-        public static int add(int a, int b)
-        {
-            return (IsOdd(a) ? a : 0) + (IsOdd(b) ? a : 0);
-        }
-        public static bool IsOdd(int a)
-        {
-            if (a % 2 == 0)
-                return true;
-            else
-                return false;
-        }
-
-        public static int multify(int a, int n )
-        {
-            return a * n;
-        }
+        
     }
 }
