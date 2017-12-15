7120a80e2404be22fe3b8f3101e135d8c0809914
Ace.Lee <daship1@gmail.com>
2017-12-15 11:31:06 +0800

    test

---
+++ ConsoleApp1/test.sql
@@ -0,0 +1,12 @@
+
+
+
+select * from Product
+select token, * from Member where Membername = 'daship'
+
+select * from MemberProduct
+
+select top 100 * from transfer order by transferid desc 
+
+
+select * from ActEvent
