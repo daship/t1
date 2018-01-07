de2cf3becefbef54d556752662cf261e5b924159
Ace.Lee <daship1@gmail.com>
2018-01-07 17:01:58 +0800

    add some comments, revise comment again

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,6 +17,10 @@ namespace ConsoleApp1
             Console.WriteLine(devide(8, 2));
             Console.Read();
             //config add but i don't want to add in conifg
+            // this is switch to ace's branch
+            //comment line 1
+            //comment line 2
+            //comment line 3
         }
         static int minus(int a, int b)
         {
